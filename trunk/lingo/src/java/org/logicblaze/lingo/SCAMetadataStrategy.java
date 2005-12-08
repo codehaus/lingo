258e431aac21f3bcaaec166d620620e8133084cd
jstrachan <jstrachan@7ef3deab-9e15-0410-9092-8930a409bf10>
2005-12-08 10:53:22 +0000

    added support for SCA annotations to denote async operations and to find callbacks

---
+++ trunk/lingo/src/java/org/logicblaze/lingo/SCAMetadataStrategy.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ * Copyright 2005 LogicBlaze, Inc. http://www.logicblaze.com
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ * 
+ **/
+package org.logicblaze.lingo;
+
+
+import org.osoa.sca.annotations.Callback;
+import org.osoa.sca.annotations.OneWay;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+
+/**
+ * An implementation of {@link MetadataStrategy} which uses
+ * the <a href="http://incubator.apache.org/tuscany/">SCA</a> 
+ * annotations to configure the metadata strategy.
+ * 
+ * @version $Revision$
+ */
+public class SCAMetadataStrategy extends CachingMetadataStrategySupport {
+
+    private static final long serialVersionUID = 1146367535939318426L;
+
+    protected MethodMetadata createMethodMetadata(Method method) {
+        OneWay annotation = method.getAnnotation(OneWay.class);
+        boolean oneway = annotation != null;
+        
+        boolean[] remoteParams = null;
+        Class[] parameterTypes = method.getParameterTypes();
+        int size = parameterTypes.length;
+        if (size > 0) {
+            remoteParams = new boolean[size];
+            for (int i = 0; i < size; i++) {
+                remoteParams[i] = isRemoteParameter(method, parameterTypes[i], i);
+            }
+        }
+        return new MethodMetadata(oneway, remoteParams);
+   }
+
+    protected boolean isRemoteParameter(Method method, Class parameterType, int index) {
+        Annotation annotation = parameterType.getAnnotation(Callback.class);
+        return annotation != null;
+    }
+}
