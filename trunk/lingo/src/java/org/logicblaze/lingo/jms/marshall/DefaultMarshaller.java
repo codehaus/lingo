ed63e70a7014529fa5465bf6bba12157e4120019
jstrachan <jstrachan@7ef3deab-9e15-0410-9092-8930a409bf10>
2005-12-21 18:13:38 +0000

    added an implementation of BlockingQueue which is distributed using JMS

--- trunk/lingo/src/java/org/logicblaze/lingo/jms/marshall/DefaultMarshaller.java
+++ trunk/lingo/src/java/org/logicblaze/lingo/jms/marshall/DefaultMarshaller.java
@@ -28,10 +28,14 @@ import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.ObjectMessage;
 import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import java.io.Serializable;
 
 /**
- * Represents the strategy of marshalling of requests and responses in and out of JMS messages
- *
+ * Represents the strategy of marshalling of requests and responses in and out
+ * of JMS messages
+ * 
  * @version $Revision$
  */
 public class DefaultMarshaller implements Marshaller {
@@ -48,15 +52,11 @@ public class DefaultMarshaller implements Marshaller {
         return message;
     }
 
-    
     public Message createResponseMessage(Session session, RemoteInvocationResult result, Message requestMessage) throws JMSException {
         ObjectMessage answer = session.createObjectMessage(result);
         addResponseMessageHeaders(answer, result, requestMessage);
         return answer;
     }
-
-
-
 
     public RemoteInvocationResult extractInvocationResult(Message message) throws JMSException {
         handleInvocationResultHeaders(message);
@@ -82,28 +82,54 @@ public class DefaultMarshaller implements Marshaller {
         return onInvalidMessage(message);
     }
 
+    public Message createObjectMessage(Session session, Object value) throws JMSException {
+        Message message = null;
+        if (value instanceof String) {
+            message = session.createTextMessage((String) value);
+        }
+        else {
+            message = session.createObjectMessage((Serializable) value);
+        }
+        appendMessageHeaders(message, session, value);
+        return message;
+    }
+
+    public Object readMessage(Message message) throws JMSException {
+        handleMessageHeaders(message);
+        if (message instanceof ObjectMessage) {
+            ObjectMessage objectMessage = (ObjectMessage) message;
+            return objectMessage.getObject();
+        }
+        else if (message instanceof TextMessage) {
+            TextMessage textMessage = (TextMessage) message;
+            return textMessage.getText();
+        }
+        return onInvalidMessage(message);
+    }
+
     // Properties
-    //-------------------------------------------------------------------------
+    // -------------------------------------------------------------------------
     public boolean isIgnoreInvalidMessages() {
         return ignoreInvalidMessages;
     }
 
     /**
-     * Sets whether invalidly formatted messages should be silently ignored or not
+     * Sets whether invalidly formatted messages should be silently ignored or
+     * not
      */
     public void setIgnoreInvalidMessages(boolean ignoreInvalidMessages) {
         this.ignoreInvalidMessages = ignoreInvalidMessages;
     }
 
     // Implementation methods
-    //-------------------------------------------------------------------------
+    // -------------------------------------------------------------------------
     protected RemoteInvocationResult onInvalidClientMessage(Message message) throws JMSException {
         throw new JMSException("Invalid response message: " + message);
     }
 
     /**
-     * Handle invalid messages by just logging, though a different implementation
-     * may wish to throw exceptions
+     * Handle invalid messages by just logging, though a different
+     * implementation may wish to throw exceptions
      */
     protected RemoteInvocation onInvalidMessage(Message message) {
         String text = "Invalid message will be discarded: " + message;
@@ -115,12 +141,16 @@ public class DefaultMarshaller implements Marshaller {
     }
 
     /**
-     * A strategy method for derived classes to allow them a plugin point to perform custom header processing
+     * A strategy method for derived classes to allow them a plugin point to
+     * perform custom header processing
      */
     protected void appendMessageHeaders(Message message, Requestor requestor, LingoInvocation invocation) throws JMSException {
         if (invocation.getMetadata().isStateful()) {
             message.setStringProperty("JMSXGroupID", getStickySessionID());
         }
+    }
+
+    protected void appendMessageHeaders(Message message, Session session, Object value) {
     }
 
     protected String getStickySessionID() {
@@ -132,23 +162,32 @@ public class DefaultMarshaller implements Marshaller {
         return stickySessionID;
     }
 
-
     /**
-     * A strategy for derived classes to allow them to plug in custom header processing for responses
+     * A strategy for derived classes to allow them to plug in custom header
+     * processing for responses
      */
     protected void addResponseMessageHeaders(ObjectMessage answer, RemoteInvocationResult result, Message requestMessage) throws JMSException {
     }
 
     /**
-     * A strategy method to allow derived classes to process the headers in a special way
+     * A strategy method to allow derived classes to process the headers in a
+     * special way
      */
     protected void handleInvocationHeaders(Message message) {
     }
 
     /**
-     * A strategy method to allow derived classes to process the headers in a special way
+     * A strategy method to allow derived classes to process the headers in a
+     * special way
      */
     protected void handleInvocationResultHeaders(Message message) {
+    }
+
+    /**
+     * A strategy method to allow derived classes to process the headers in a
+     * special way
+     */
+    protected void handleMessageHeaders(Message message) {
     }
 
 }
