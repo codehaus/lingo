e40e4f990d2f15cd36175a3a16563b5f600457f5
sjivan <sjivan@7ef3deab-9e15-0410-9092-8930a409bf10>
2006-07-14 11:02:17 +0000

    Issue LINGO-18 : Client Context propagation

---
+++ trunk/lingo/src/main/java/org/logicblaze/lingo/ClientContextImpl.java
@@ -0,0 +1,27 @@
+package org.logicblaze.lingo;
+
+/**
+ * Default implmentation of {@link ClientContext}. USed by {@link ClientContextHolder}.
+ *
+ * @author Sanjiv Jivan
+ * @since 1.5
+ */
+public class ClientContextImpl implements ClientContext {
+
+    private String userName;
+
+    public ClientContextImpl() {
+    }
+
+    public ClientContextImpl(String userName) {
+        this.userName = userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+}
